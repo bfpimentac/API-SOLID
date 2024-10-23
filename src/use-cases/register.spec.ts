9f5d9bab5957205ef68fdd2e8d4f402062b7512e
bfpimentac <beatriz.freire.pimenta@gmail.com>
2024-10-23 14:39:39 -0300

    test: implement test to verify user password is hashed

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,4 +1,38 @@
-import { expect, test } from 'vitest'
-test('check if it works', () => {
-    expect(2+2).toBe(4)
+import { expect,  describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+describe('Register User Case', () => {
+    it('should hash user password upon registration', async () => {
+        const registerUseCase = new RegisterUseCase({
+
+            async findByEmail(email){
+                return null
+            },
+
+            async create(data) {
+                return {
+                    id: 'user-1',
+                    name: data.name,
+                    email: data.email,
+                    password_hash: data.password_hash,
+                    createdAt: new Date()
+
+                }
+            },
+        })
+
+        const { user } = await registerUseCase.execute({
+            name: "John Doe",
+            email: "johndoe1@example.com",
+            password: "123456",
+        })
+
+        const isPasswordCorrectlyHashed = await compare(
+            '123456',
+            user.password_hash
+        )
+
+        expect(isPasswordCorrectlyHashed).toBe(true)
+    }
+)
 })
